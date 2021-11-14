cc140c82c9bdf169573c08fe2149ab622366fd44
Avi <47988220+avishein21@users.noreply.github.com>
2021-11-14 12:11:53 -0500

    Fixed timer, scene 2 follows player no

--- ExamNight/Assets/Scripts/Collide.cs
+++ ExamNight/Assets/Scripts/Collide.cs
@@ -7,39 +7,18 @@ using UnityEngine.UI;
 public class Collide : MonoBehaviour
 {
     public GameObject enemy;
-    private int gameTime = 20;
-    private float gameTimer = 0f;
-    public GameObject timeText;
     public Rigidbody2D rb;
 
 
     // Start is called before the first frame update
     void Start() {
         Physics2D.IgnoreLayerCollision(7, 8);
-        UpdateTime();
     }
 
     void Update() {
         
     }
     
-    void FixedUpdate(){
-      gameTimer += 0.02f;
-      if (gameTimer >= 1f){
-                  gameTime -= 1;
-                  gameTimer = 0;
-                  UpdateTime();
-      }
-      if (gameTime <= 0){
-                  gameTime = 0;
-                  SceneManager.LoadScene("ExamMiss");                                                   // restart same level
-      }
-  }
-  
-  public void UpdateTime(){
-      Text timeTextB = timeText.GetComponent<Text>();
-      timeTextB.text = "TIME: " + gameTime;
-  } 
     
     //Collect note
     void OnCollisionEnter2D(Collision2D other){
--- ExamNight/Assets/Scripts/Follow.cs
+++ ExamNight/Assets/Scripts/Follow.cs
@@ -1,14 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class Follow : MonoBehaviour
 {
     public Transform followTransform;
-
+    public static float gameTime = 20;
+    private float gameTimer = 0f;
+    public GameObject timeText;
+    
+    void Start(){
+        UpdateTime();
+    }
+    
     void FixedUpdate()
     {
         this.transform.position = new Vector3(followTransform.position.x, followTransform.position.y, this.transform.position.z);
+        gameTimer += 0.02f;
+        if (gameTimer >= 1f){
+                    gameTime -= 1f;
+                    gameTimer = 0;
+                    UpdateTime();
+        }
+        if (gameTime <= 0){
+                    gameTime = 0;
+                    SceneManager.LoadScene("ExamMiss");                                                   // restart same level
+        }
     }
+    
+    public void UpdateTime(){
+        Text timeTextB = timeText.GetComponent<Text>();
+        timeTextB.text = "TIME: " + gameTime;
+    } 
         
 }
