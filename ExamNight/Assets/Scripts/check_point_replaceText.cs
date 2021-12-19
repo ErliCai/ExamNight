e0db9348a7795c05b8c234e04bf96f08f5093f78
ErliCai <caierli123@gmail.con>
2021-12-19 03:47:49 -0500

    Teacher Angry Face

--- ExamNight/Assets/Scripts/check_point_replaceText.cs
+++ ExamNight/Assets/Scripts/check_point_replaceText.cs
@@ -9,15 +9,45 @@ public class check_point_replaceText : MonoBehaviour
     public GameObject ttext;
     public string new_string;
     public bool kickedout = false;
+    public bool angryFace = false;
+    public GameObject angryteacher;
+    public GameObject happyteacher;
+    public static bool angry = false;
+    
+    void Start(){
+      angryteacher = GameObject.FindWithTag("AngryTeacher");
+      happyteacher = GameObject.FindWithTag("HappyTeacher");
+    }
+    
+    void FixedUpdate(){
+      if (angry) {
+        angryteacher.SetActive(true);
+        happyteacher.SetActive(false);
+      }
+      else{
+        happyteacher.SetActive(true);
+        angryteacher.SetActive(false);
+      }
+    }
     
     void OnTriggerEnter2D(Collider2D other){
       if (other.gameObject.tag.Equals("Player")){
         Debug.Log("reaching checkpoint");
         ttext.GetComponent<Text>().text = new_string;
+        
+        if (angryFace) {
+          angry = true;
+        }
+        else{
+          angry = false;
+        }
+        
       }
       
       if (kickedout){
         SceneManager.LoadScene("Start"); 
       }
+      
     }
+    
 }
